b2b73aca8f994b8ff1550d81f5a5edf8bd93bee2
praveen-me <praveenkrsaini0@gmail.com>
2018-10-10 15:59:24 +0530

    Add Stack and Queue Folder

---
+++ 2. Queue/exercises.js
@@ -0,0 +1,34 @@
+// 1. Implement Queue Data Structure using String
+// Operations you need to add are
+// isEmpty() - returns true if the storage is empty
+// enqueue(value) - add a new value and returns the size of the storage
+// dequeue() - remove one element and returns the deleted element
+// peek() - displays the latest element in the storage
+// size() - the size of the storage
+
+class Queue {
+  constructor() {
+  }
+  isEmpty() {
+  }
+  enqueue(value) {
+  }
+  dequeue() {
+  }
+  peek() {
+  }
+  size() {
+  }
+}
+
+// Implement all above methods using Object data type
+// Additionals:
+// Modify your queue to take max capacity as a parameter and once you exceed the size it should print message
+// "Max capacity already reached"
+
+
+// Make a method named 'contains' which will return true if any of the value matches the value of object.
+// queue.contains('hello') - true or false
+
+// Write a method named sort() which sorts by value
+
